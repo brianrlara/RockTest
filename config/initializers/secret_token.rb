b2ccf00f99565636eb3fe7f201a601c2b2e2bc11
Brian Lara <brian@192.168.1.105>
2012-07-07 21:19:19 -0400

    MASTER init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AS3T::Application.config.secret_token = '59ef4e8f8c7a1cf1f51b910bd7b19230294427d1e1ce3dff7a12b345c5957cec9ff1bdaeaaff64f718c9937b3f40ad0afc949b18058445063f9a4969916d7089'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AS3T::Application.config.session_store :cookie_store, key: '_AS3_T_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AS3T::Application.config.session_store :active_record_store
